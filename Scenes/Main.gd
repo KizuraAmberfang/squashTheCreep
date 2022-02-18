ff334fb3efb2a3f1e5bacc7e5c7aed4d3f6cfca5
Giulia Di lorenzo <gdi-lore@e3r10p3.42roma.it>
2022-02-18 15:07:22 +0100

    ottava lezione

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -4,13 +4,8 @@ export (PackedScene) var mob_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$UserInterface/Retry.hide()
 	randomize()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Timer_timeout():
 	var mob = mob_scene.instance()
@@ -19,7 +14,13 @@ func _on_Timer_timeout():
 	var player_position = $Player.transform.origin
 	add_child(mob)
 	mob.initialize(mob_spawn_location.translation, player_position)
+	mob.connect("squashed", $UserInterface/ScoreLabel, "_on_Mob_squashed")
 
 
 func _on_Player_hit():
 	$Timer.stop()
+	$UserInterface/Retry.show()
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		get_tree().reload_current_scene()
